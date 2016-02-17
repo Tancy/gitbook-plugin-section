fac429fa27c84cec14a9c4b93cdc54417bedfc0d
晓田 <tianxue.nitx@taobao.com>
2016-02-17 17:54:12 +0800

    add files

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # gitbook-plugin-section
+
+This plugin will add table of content to the page and provide navigation function inside a page.
+
+Add `<!-- toc -->` to the markdown files. When you build the book, it will insert a table of content where you insert `<!-- toc -->`
+
+
+`book.json` Config:
+
+
+```
+{
+	"plugins": ["section"],
+	"pluginsConfig": {
+		"section": {
+			"addClass": true,
+			"className": "tsection"
+		}
+	}
+}
+```
+
+You can add this config to add a HTML ClassName to the Section `ul` element
+
+<img src="https://gw.alicdn.com/tps/TB1YiC4LFXXXXcrXFXXXXXXXXXX-1908-1126.jpg" width="640px">
---
+++ index.js
@@ -0,0 +1,19 @@
+var toc = require('markdown-toc');
+
+module.exports = {
+    book: {},
+    hooks: {
+        "page:before": function (page) {
+            page.content = toc.insert(page.content, {
+                slugify: function (str) {
+                    return encodeURI(str.toLowerCase().replace(/[\\!"#$%&'()*+,.\/:;<=>?@[\]^_`{|}~-]/g,'')).replace(/%20/g, '-');
+                }
+            });
+            if (this.options.pluginsConfig.atoc.addClass) {
+                var className = this.options.pluginsConfig.atoc.className || 'tsection';
+                page.content = page.content + '<style>.book .book-summary ul.summary li li.active ul li{list-style:square inside;}</style>>\n\n\n<script type="text/javascript">var targetUl = document.getElementsByClassName(\'page-inner\')[0].getElementsByTagName(\'ul\')[0];if(targetUl&&targetUl.getElementsByTagName(\'a\').length>0){targetUl.className=\'' + className + '\';} document.getElementsByClassName(\'chapter active\')[0].appendChild(document.getElementsByClassName(\'atoc\')[0].getElementsByTagName(\'ul\')[0]);document.getElementsByClassName(\'atoc\')[0].remove()</script>';
+            }
+            return page;
+        }
+    }
+};
