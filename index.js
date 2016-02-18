f30dae6521abbe06f450234541b182600fe820ec
晓田 <tianxue.nitx@taobao.com>
2016-02-18 14:31:46 +0800

    fixbug

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
             });
             if (this.options.pluginsConfig.atoc.addClass) {
                 var className = this.options.pluginsConfig.atoc.className || 'tsection';
-                page.content = page.content + '<style>.book .book-summary ul.summary li li.active ul li{list-style:square inside;}</style>>\n\n\n<script type="text/javascript">var targetUl = document.getElementsByClassName(\'page-inner\')[0].getElementsByTagName(\'ul\')[0];if(targetUl&&targetUl.getElementsByTagName(\'a\').length>0){targetUl.className=\'' + className + '\';} document.getElementsByClassName(\'chapter active\')[0].appendChild(document.getElementsByClassName(targetUl.className)[0].getElementsByTagName(\'ul\')[0]);document.getElementsByClassName(targetUl.className)[0].remove()</script>';
+                page.content = page.content + '<style>.book .book-summary ul.summary li li.active ul li{list-style:square inside;}</style>>\n\n\n<script type="text/javascript">var targetUl = document.getElementsByClassName(\'page-inner\')[0].getElementsByTagName(\'ul\')[0];if(targetUl&&targetUl.getElementsByTagName(\'a\').length>0){targetUl.className=\'' + className + '\';} document.getElementsByClassName(\'chapter active\')[0].appendChild(document.getElementsByClassName(targetUl.className)[0].getElementsByTagName(\'ul\')[0]);document.getElementsByClassName(targetUl.className)[0].remove();setTimeout(function(){$(document).unbind(\'click\');},1000)</script>';
             }
             return page;
         }
