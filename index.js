dba3fe7c485fc4dd7a10666f3de8c765df7311db
Doug Daniels <daniels.douglas@gmail.com>
2016-02-18 21:37:19 -0600

    config should be section not atoc
    was getting an error:
    
    ```
    TypeError: Cannot read property 'addClass' of undefined (In file 'README.md')
    ```
    
    my `book.json` was:
    ```
    {
        "plugins": ["section"],
        "pluginsConfig": {
            "section": {
                "addClass": true,
                "className": "tsection"
            }
        }
    }
    ```
    
    Also this would fail if the config didn't specify `atoc` at all I think it should work without a config.

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ module.exports = {
                     return encodeURI(str.toLowerCase().replace(/[\\!"#$%&'()*+,.\/:;<=>?@[\]^_`{|}~-]/g,'')).replace(/%20/g, '-');
                 }
             });
-            if (this.options.pluginsConfig.atoc.addClass) {
-                var className = this.options.pluginsConfig.atoc.className || 'tsection';
+            if (this.options.pluginsConfig.section && this.options.pluginsConfig.section.addClass) {
+                var className = this.options.pluginsConfig.section.className || 'tsection';
                 page.content = page.content + '<style>.book .book-summary ul.summary li li.active ul li{list-style:square inside;}</style>>\n\n\n<script type="text/javascript">var targetUl = document.getElementsByClassName(\'page-inner\')[0].getElementsByTagName(\'ul\')[0];if(targetUl&&targetUl.getElementsByTagName(\'a\').length>0){targetUl.className=\'' + className + '\';} document.getElementsByClassName(\'chapter active\')[0].appendChild(document.getElementsByClassName(targetUl.className)[0].getElementsByTagName(\'ul\')[0]);document.getElementsByClassName(targetUl.className)[0].remove();setTimeout(function(){$(document).unbind(\'click\');},1000)</script>';
             }
             return page;
